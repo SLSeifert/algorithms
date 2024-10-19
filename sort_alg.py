3424c6b9ee37c1972b1e11ed6334f0e1c9d11664
SLSeifert <slseifert1986@gmail.com>
2024-10-19 09:46:51 -0700

    sort_alg.py

---
+++ sort_alg.py
@@ -0,0 +1,22 @@
+#simple sorting algorithm
+def findSmallest(arr):
+    smallest = arr[0]
+    smallest_index = 0
+    for i in range(1, len(arr)):
+        if arr[i] < smallest:
+            smallest = arr[i]
+            smallest_index = i
+    return smallest_index
+
+def selectionSort(arr):
+    newArr = []
+    copiedArr = list(arr)
+    for i in range(len(copiedArr)):
+        smallest = findSmallest(copiedArr)
+        newArr.append(copiedArr.pop(smallest))
+    return newArr
+
+MyArray = [7,3,2,9,4,11]
+
+print(findSmallest(MyArray))
+print(selectionSort(MyArray))
